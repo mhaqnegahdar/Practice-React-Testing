ebcadd932d74b5f34bb2a87fd8819e131e73a828
max jn <maxjn763@gmail.com>
2023-07-09 20:07:16 +0330

    Important Query Methods (Priority Order):
    - Introduction to **geByPlaceholder**
    -**getByLabelText**-**getByDisplayValue**
    -**getByAltText**
    -**getByTitle**
    -**getByTestId** gets matching element from custom attribure like data-testId, use: with dynamic data

--- __test__/src/components/application/Application.test.tsx
+++ __test__/src/components/application/Application.test.tsx
@@ -12,32 +12,50 @@ describe("Application", () => {
 
     // Check MainHeading is rendered
     const mainHeading = screen.getByRole("heading", { level: 1 });
-
     expect(mainHeading).toBeInTheDocument();
 
     // Check NameTextBox is rendered
     const secondHeading = screen.getByRole("heading", { level: 2 });
-
     expect(secondHeading).toBeInTheDocument();
 
     // Check NameTextBox is rendered
     const nameTextBox = screen.getByRole("textbox", { name: "Name" });
-
     expect(nameTextBox).toBeInTheDocument();
+
+    // Check NameTextBox is rendered
+    const nameTextBox2 = screen.getByLabelText("Name");
+    expect(nameTextBox2).toBeInTheDocument();
+
+    // Check NameTextBox is rendered
+    const nameTextBox3 = screen.getByPlaceholderText("Fullname");
+    expect(nameTextBox3).toBeInTheDocument();
+
+    // Check NameTextBox is rendered
+    const nameTextBox4 = screen.getByDisplayValue("Vishwas");
+    expect(nameTextBox4).toBeInTheDocument();
+
+    // Check Image is rendered
+    const image = screen.getByAltText("a person with a laptop");
+    expect(image).toBeInTheDocument();
+
+    // Check Image is rendered
+    const close = screen.getByTitle("close");
+    expect(close).toBeInTheDocument();
+
+    // Check element with custom attribute is rendered
+    const custom = screen.getByTestId("custom-element");
+    expect(custom).toBeInTheDocument();
 
     // Check ComboBox is rendered
     const comboBox = screen.getByRole("combobox");
-
     expect(comboBox).toBeInTheDocument();
 
     // Check terms element is rendered
     const termsElement = screen.getByRole("checkbox");
-
     expect(termsElement).toBeInTheDocument();
 
     // Check submit button is rendered
     const submitButton = screen.getByRole("button");
-
     expect(submitButton).toBeInTheDocument();
   });
 });
